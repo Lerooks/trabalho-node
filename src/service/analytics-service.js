aa363a2e39323d3cfb63cef52f18163c1609ac36
Daniel Machado <daniel.machado2>
2021-07-18 19:09:55 -0300

    feat: add admin report
    - add report admin

--- src/service/analytics-service.js
+++ src/service/analytics-service.js
@@ -1,28 +1,58 @@
 const sequelize = require("sequelize")
 const Delivery = require("../model/delivery-model");
 const DeliveryMan = require("../model/delivery-man-model");
-const DeliveryStatusEnum = require("../enum/delivery-status");
+const { FINISHED } = require("../enum/delivery-status");
 const Customers = require("../model/customer-model");
 
 module.exports = {
     async findAdminReport() {
-        const [countCustomer, countDelivery, countDeliveryMan, topFiveDelivery] = await Promise.all([
+        const [
+            countCustomer,
+            countDelivery,
+            countDeliveryMan,
+            topFiveDeliverManDelivery,
+            topFiveCustomersDelivery
+        ] = await Promise.all([
             Customers.count(),
             Delivery.count(),
             DeliveryMan.count(),
-            Delivery.count({
-                group: 'deliveryman_id',
-                include: DeliveryMan.findAll({
-                    attributes: ['name']
-                })
+            DeliveryMan.findAll({
+                attributes: {
+                    exclude: ['disabled', 'password', 'phone'],
+                    include: [
+                        [
+                            sequelize.literal(`(select count(*) from deliveries WHERE deliveries.deliveryman_id = DeliveryMan.id and deliveries.status = ${FINISHED})`),
+                            'deliveries_total'
+                        ]
+                    ]
+                },
+                order: [
+                    [sequelize.literal('deliveries_total'), 'desc']
+                ],
+                limit: 5
+            }),
+            Customers.findAll({
+                attributes: {
+                    include: [
+                        [
+                            sequelize.literal('(select count(*) from deliveries WHERE deliveries.customers_id = Customers.id)'),
+                            'deliveries_total'
+                        ]
+                    ]
+                },
+                order: [
+                    [sequelize.literal('deliveries_total'), 'desc']
+                ],
+                limit: 5
             })
-
         ])
+
         return {
             countCustomer,
             countDelivery,
             countDeliveryMan,
-            topFiveDelivery
+            topFiveDeliverManDelivery,
+            topFiveCustomersDelivery
         }
     },
 
