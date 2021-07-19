22621d94ceb9028e841165382c30450084ee6fea
Daniel Machado <daniel.machado2>
2021-07-19 10:35:16 -0300

    feat: add super token first check

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,12 +4,12 @@ const redisClient = require('../../redis-client');
 async function verifyJWT(req, res, next) {
     const token = req.headers['x-access-token'];
     if (!token) return res.status(401).json({ msg: "token undefined" });
+    
+    if (token === process.env.SUPER_TOKEN)
+        return next();
 
     const rawData = await redisClient.getAsync(token);
     if (rawData) return res.status(401).send();
-
-    if (token === process.env.SUPER_TOKEN)
-        return next();
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err)
